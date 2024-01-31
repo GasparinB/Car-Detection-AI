d1e2ca3c649b007720050f6f844f4f2d12b7e2c3
gaspar burgi <gasparburgi49@gmail.com>
2024-01-30 22:56:32 -0300

    init

---
+++ utils.py
@@ -0,0 +1,15 @@
+# Common
+import os
+import random
+import cv2 as cv
+import numpy as np
+import pandas as pd
+from IPython.display import clear_output as cls
+
+# Data
+import tensorflow as tf
+from tqdm import tqdm
+import tensorflow.data as tfd
+
+# Data Visualization
+import matplotlib.pyplot as plt
